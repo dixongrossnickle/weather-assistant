1b50180e0f3b11dfccdea9aac79d01fc8bcdcd22
Dixon Grossnickle <69313597+dixongrossnickle@users.noreply.github.com>
2021-11-15 14:45:06 -0600

    formatting

--- src/app.py
+++ src/app.py
@@ -3,14 +3,14 @@ import sys
 from weather import *
 
 HELP_MESSAGE = "> weather.py <arg>\nValid args:"
-VALID_ARGS = ['hourly', 'nightly',]
+VALID_ARGS = ['hourly', 'nightly', ]
 for arg in VALID_ARGS:
     HELP_MESSAGE += ('\n--' + arg)
 
 
 def main(argv: list[str]):
     if len(argv) == 0:
-        print('INVALID COMMAND: 1 argument required.\n' + HELP_MESSAGE)
+        print('ERROR: 1 argument permitted (required).\n' + HELP_MESSAGE)
         sys.exit(2)
     arg = argv[0]
     match arg:
@@ -19,7 +19,7 @@ def main(argv: list[str]):
         case '--nightly':
             arg = 'n'
         case _:
-            print('INVALID ARGUMENT\n'+HELP_MESSAGE)
+            print('INVALID ARGUMENT\n' + HELP_MESSAGE)
             sys.exit(2)
 
     wa = WeatherAssistant()
@@ -27,7 +27,7 @@ def main(argv: list[str]):
         notification = wa.hourly_check()
         if notification != '':
             wa.send_sms(notification)
-    
+
     elif arg == 'n':
         notification = wa.nightly_check()
         if notification != '':
