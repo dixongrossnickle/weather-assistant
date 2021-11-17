a1d6508bf299c9b3dbd1165107485b9d31de1d74
Dixon Grossnickle <69313597+dixongrossnickle@users.noreply.github.com>
2021-11-16 19:46:32 -0600

    comments

--- src/run.py
+++ src/run.py
@@ -9,23 +9,24 @@ for arg in VALID_ARGS:
 
 
 def main(argv: list[str]):
+    # Check number of arguments
     if len(argv) != 1:
         print(
             f"Invalid Argument Count: 1 argument {'permitted' if len(argv) else 'required'}.\n"
             + HELP_MESSAGE)
         sys.exit(2)
-
+    # Check argument
     arg = argv[0].removeprefix('--')
     if arg not in VALID_ARGS:
         print(f"Invalid Argument: {arg}\n" + HELP_MESSAGE)
         sys.exit(2)
-
+    # Check the weather
     wa = WeatherAssistant()
     if arg == 'hourly':
         notification = wa.hourly_check()
     elif arg == 'nightly':
         notification = wa.nightly_check()
-
+    # Check notification
     if notification != '':
         wa.send_sms(notification)
 
