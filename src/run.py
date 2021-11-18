15e52df8a423c39cb6216abd48e97aeca2d4dc89
Dixon Grossnickle <dixon.grossnickle@gmail.com>
2021-11-18 12:21:22 -0600

    update help msg

--- src/run.py
+++ src/run.py
@@ -2,23 +2,22 @@
 import sys
 from weather import *
 
-HELP_MESSAGE = ">>> python run.py <arg>\nValid args:"
 VALID_ARGS = ('hourly', 'nightly', )
-for arg in VALID_ARGS:
-    HELP_MESSAGE += ('\n--' + arg)
+
+HELP_MESSAGE = (">>> python run.py <arg>\nValid args:\n" +
+                '\n'.join([f'--{arg}' for arg in VALID_ARGS]))
 
 
 def main(argv: list[str]):
     # Check number of arguments
     if len(argv) != 1:
-        print(
-            f"Invalid Argument Count: 1 argument {'permitted' if len(argv) else 'required'}.\n"
-            + HELP_MESSAGE)
+        print(f"Invalid Argument Count: 1 argument {'permitted' if len(argv) else 'required'}.",
+              HELP_MESSAGE, sep='\n')
         sys.exit(2)
     # Parse & validate argument
     arg = argv[0].removeprefix('--')
     if arg not in VALID_ARGS:
-        print(f"Invalid Argument: {arg}\n" + HELP_MESSAGE)
+        print(f"Invalid Argument: {arg}", HELP_MESSAGE, sep='\n')
         sys.exit(2)
     # Check the weather
     wa = WeatherAssistant()
